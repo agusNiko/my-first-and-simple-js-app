d41eb0644a24b957b0065ef915f8b67a6f6e2966
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-02-06 10:40:36 +0100

    Creating the pokedex
    this commit Create a new variable called pokemonList and objects with the names of the pokemos. it also pushes the pokemos into the pokemonList array

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
-alert('Hi Eveline! i´m glad to be back! :D')
 
-let favoriteFood = 'Venezuelan Empanadas,  '
-
-document.write(favoriteFood);
+let pokemonList = []; //I defined the variable pokemon list as an blank Array
 
 
-document.write('this is an Empanada: <img src="css/empanadas.jpg" alt="a picture of an Empanada" width="300px">');
+// I created different variables with the name of the Pokémon, then assigned them a object with its different properties.
+let pikachu =  {
+    name: 'Pikachu', 
+    height:  0.4, type: 'electric', 
+    abilities: ['Static', 'Lightningrod']
+}; 
+
+let bulbasaur=  {
+    name: 'Bulbasaur', 
+    height:  0.7, 
+    type: ['grass', 'poison'], 
+    abilities: ['Chlorophyll', 'Overgrow']};
+
+let charmander=  {
+    name: 'Charmander', 
+    height:  0.6, 
+    type: 'fire', 
+    abilities: ['Blaze', 'Solar-power']};
+
+let squirtle=  {
+    name: 'Squirtle', 
+    height:  0.5, 
+    type: 'water', 
+    abilities: ['Rain-dish', 'Torrent']
+};
+
+
+
+pokemonList.push(pikachu, bulbasaur, charmander, squirtle); //with the function .push() I pushed the pokemons into the Aray pokemonList;
+
+
+console.log(pokemonList);
+
+
