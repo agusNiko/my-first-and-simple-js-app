9076becbcd315c55bae3cb5c675bdf849682c502
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-02-07 20:59:05 +0100

    creates basics loop and conditional
    this commits generate a  loop that write the name of the pokemos and their height in the DOM also uses conditional to determinate the pokemon with a height < or = to 0.3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 
 let pokemonList = []; //I defined the variable pokemon list as an blank Array
 
-
 // I created different variables with the name of the Pokémon, then assigned them a object with its different properties.
 let pikachu =  {
     name: 'Pikachu', 
@@ -9,30 +8,49 @@ let pikachu =  {
     abilities: ['Static', 'Lightningrod']
 }; 
 
-let bulbasaur=  {
+let bulbasaur =  {
     name: 'Bulbasaur', 
     height:  0.7, 
     type: ['grass', 'poison'], 
-    abilities: ['Chlorophyll', 'Overgrow']};
+    abilities: ['Chlorophyll', 'Overgrow']
+};
 
-let charmander=  {
+let charmander =  {
     name: 'Charmander', 
     height:  0.6, 
     type: 'fire', 
-    abilities: ['Blaze', 'Solar-power']};
+    abilities: ['Blaze', 'Solar-power']
+};
 
-let squirtle=  {
+let squirtle =  {
     name: 'Squirtle', 
     height:  0.5, 
     type: 'water', 
     abilities: ['Rain-dish', 'Torrent']
 };
 
+let caterpie = {
+    name: 'Caterpie', 
+    height:  0.3, 
+    type: 'bug', 
+    abilities: ['Shield-dust', 'Run-away']
+};
+
+pokemonList.push(bulbasaur, charmander, squirtle); //with the function .push() I pushed the pokemons into the Aray pokemonList;
+
+pokemonList.unshift(pikachu); // similar to push but adds the element at the biginnin
+
+pokemonList[4] = caterpie; // this comand adds an element at the specified index [4] of the array;
+
+console.log(pokemonList); 
 
 
-pokemonList.push(pikachu, bulbasaur, charmander, squirtle); //with the function .push() I pushed the pokemons into the Aray pokemonList;
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height <= 0.3 ) {
+        document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}m) - So cute! That's a really tiny Pokémon! </p>`);   
+    } else {
+    document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}m)</p>`);    
+    }
+  } //this loop write the name of the pokemos and their height in the DOM also uses conditional to determinate the pokemon with a height < or = to 0.3
 
-
-console.log(pokemonList);
-
-
+  
